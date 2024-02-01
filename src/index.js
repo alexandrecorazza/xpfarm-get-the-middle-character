6712a35c9bbf72e63af0ccb825bda401fa8671dc
Alexandre Corazza Marcolino <alexandre_corazza@yahoo.com.br>
2024-01-31 23:12:35 -0300

    third test

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ const middleChar = (string) => {
   if (!string || string.length < 0 || string.length > 1000) return false;
   else if (string.length % 2 != 0) {
     return string[Math.floor(string.length / 2)]
+  } else {
+    let auxString = '';
+    auxString = string[(string.length / 2) - 1] + string[(string.length / 2)];
+    return auxString;
   }
 };
   
--- tests/index.test.js
+++ tests/index.test.js
@@ -7,3 +7,11 @@ test("should return false if the string length is not between 0 and 1000", () =>
 test("should return 't' if string is 'testing'", () => {
     expect(middleChar('testing')).toBe('t')
 });
+
+test("should return 'es' if string is 'test'", () => {
+    expect(middleChar('test')).toBe('es')
+});
+
+test("should return 'dd' if string is 'middle'", () => {
+    expect(middleChar('middle')).toBe('dd')
+});
